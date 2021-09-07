5fb0c46f5c71a451e8b70379432f3bb5f8cc1042
giov <pintogiovanni818@gmail.com>
2021-09-07 16:56:27 +0200

    incompleto

--- vue/main.js
+++ vue/main.js
@@ -7,16 +7,22 @@ Vue.config.devtools = true;
 const app = new Vue( {
     el1: "#root",
         data: {
-            message: "ciao"
+            message: "ciao",
+            src: ('./vue/img/img.jpg') ,
+            url: ('https://github.com/Jhon818/vue-hello/blob/f1412ebf2b508b6e66922333646ba86772e33176/vue/img/img.jpg') 
     },
-    
-    el2: ".img",
-    data1: {
-        src: "./img/img.jpg"
-    },
+    createElment: "img",
+    data: {
+        src: ('./vue/img/img.jpg') 
+      },
+      methods: {
+        myFunction: function () {
+            (this.src === './vue/img/img.jpg' ? './vue/img/img.jpg' : this.url);
+        }
+    }
 
 
 })
-console.log(app.message);
+console.log(Vue);
 // Bonus:
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
