8b6547386bb79e5c88c83bfc58a2ec67927b882e
giov <pintogiovanni818@gmail.com>
2021-09-08 12:54:52 +0200

    completato

--- vue/main.js
+++ vue/main.js
@@ -5,16 +5,12 @@ Vue.config.devtools = true;
 // console.log(new_row)
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 const app = new Vue( {
-    el1: "#root",
+    el: "#root",
         data: {
             message: "ciao",
-            src: ('./vue/img/img.jpg') ,
+            src: ('../vue/img/img.jpg') ,
             url: ('https://github.com/Jhon818/vue-hello/blob/f1412ebf2b508b6e66922333646ba86772e33176/vue/img/img.jpg') 
     },
-    createElment: "img",
-    data: {
-        src: ('./vue/img/img.jpg') 
-      },
       methods: {
         myFunction: function () {
             (this.src === './vue/img/img.jpg' ? './vue/img/img.jpg' : this.url);
